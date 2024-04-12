8212201563f1660eb87d0b41cf35224eee857bfd
Jushwanth <76085349+JushwanthGV@users.noreply.github.com>
2024-04-12 12:02:20 +0530

    first commit

--- src/pages/Authentication/Login.jsx
+++ src/pages/Authentication/Login.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useState } from "react";
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import { TextField } from "@mui/material";
 
+const validationSchema = {};
+const initialValues = {};
 const Login = () => {
+  const [formValue, setFormValue] = useState();
+  const handleSubmit = () => {
+    console.log("handle submit");
+  };
   return (
-    <div>
-      Login
-    </div>
-  )
-}
+    <>
+      <Formik
+        onSubmit={handleSubmit}
+        validationSchema={validationSchema}
+        initialValues={initialValues}
+      >
+        <Form className="space-y-5">
+          <div className="space-y-5">
+            <div>
+              <Field
+                as={TextField}
+                name="email"
+                placeholder="Email"
+                type="email"
+                variant="outlined"
+                fullWidth
+              />
+              <ErrorMessage name="email" component={"div"} className="text-red-500"/>
+            </div>
+          </div>
+        </Form>
+      </Formik>
+    </>
+  );
+};
 
-export default Login
+export default Login;
