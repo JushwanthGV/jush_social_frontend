aa178f2e9f776f0bb85950f3f0549d66869da6d2
Jushwanth <76085349+JushwanthGV@users.noreply.github.com>
2024-04-17 04:46:05 +0530

    few changes in message

--- src/pages/Message/Message.jsx
+++ src/pages/Message/Message.jsx
@@ -1,29 +1,31 @@
 import { Avatar, Grid, IconButton } from "@mui/material";
 import React from "react";
 import WestIcon from "@mui/icons-material/West";
-import AddIcCallIcon from '@mui/icons-material/AddIcCall';
-import VideoCallIcon from '@mui/icons-material/VideoCall';
+import AddIcCallIcon from "@mui/icons-material/AddIcCall";
+import VideoCallIcon from "@mui/icons-material/VideoCall";
+import AddPhotoAlternateIcon from '@mui/icons-material/AddPhotoAlternate';
+
 
 const Message = () => {
+  const handleSelectImage = () => {
+    console.log("handle select image.....");
+  };
   return (
     <div>
       <Grid container className="h-screen overflow-y-hidden">
         <Grid className="px-5" item xs={3}>
           <div className="flex h-full justify-between space-x-2">
             <div className="w-full">
-            <div className="flex space-x-4 items-center py-5">
-              <WestIcon />
-              <h1 className="text-xl font-bold">Home</h1>
-            </div>
-            <div className="h-[83vh]">
-              <div className="">
-                searchUser
+              <div className="flex space-x-4 items-center py-5">
+                <WestIcon />
+                <h1 className="text-xl font-bold">Home</h1>
               </div>
-              <div className="h-full space-y-4 mt-5 overflow-y-scroll hideScrollbar">
-                UserChatCard
-
+              <div className="h-[83vh]">
+                <div className="">searchUser</div>
+                <div className="h-full space-y-4 mt-5 overflow-y-scroll hideScrollbar">
+                  UserChatCard
+                </div>
               </div>
-            </div>
             </div>
           </div>
         </Grid>
@@ -31,16 +33,15 @@ const Message = () => {
           <div>
             <div className="flex justify-between items-center border-1 p-5">
               <div className="flex items-center space-x-3">
-                <Avatar src="https://cdn.pixabay.com/photo/2024/03/25/14/29/ai-generated-8654868_1280.png"/>
+                <Avatar src="https://cdn.pixabay.com/photo/2024/03/25/14/29/ai-generated-8654868_1280.png" />
                 <p>Pradeep</p>
-
               </div>
               <div className="flex space-x-3">
                 <IconButton>
-                  <AddIcCallIcon/>
+                  <AddIcCallIcon />
                 </IconButton>
                 <IconButton>
-                  <VideoCallIcon/>
+                  <VideoCallIcon />
                 </IconButton>
               </div>
             </div>
@@ -49,7 +50,25 @@ const Message = () => {
             </div>
           </div>
           <div className="sticky bottom-0 border-l">
-            messageInput
+            <div className="py-5 flex items-center justify-center space-x-5">
+              <input
+                className="bg-transparent border border-[#3b40544] rounded-full w-[90%] py-3 px-5"
+                placeholder="Type message.."
+                type="text"
+              />
+              <div>
+                <input
+                  type="file"
+                  accept="image/*"
+                  onChange={handleSelectImage}
+                  className="hidden"
+                  id="image-input"
+                />
+                <label htmlFor="image-input">
+                    <AddPhotoAlternateIcon/>
+                </label>
+              </div>
+            </div>
           </div>
         </Grid>
       </Grid>
